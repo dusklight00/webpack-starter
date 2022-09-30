4f36f83b25ed13d25b2f8268ba625520556f0937
dusklight00 <r.rahul.developer@gmail.com>
2022-09-30 17:47:17 +0530

    removed unwanted files

--- src/app/utils/input-validator.js
+++
@@ -1,7 +0,0 @@
-export const isInputValid = (...inputs) => {
-  const isValid = inputs.every((input) => {
-    const inputInt = parseInt(input);
-    return typeof inputInt === 'number' && !isNaN(input);
-  });
-  return isValid;
-};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { run } from './app/app';
-import './main.scss';
-run();
+import { Chess } from 'chess.js';
+
+const chess = new Chess(
+  'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - c3 0 19'
+);
+
+const board = chess.ascii();
+console.log(board);
